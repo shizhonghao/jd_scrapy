bde913e1a75d3cbe332e9e20c03e0168ed8e28dc
yang rouman <rouman321@163.com>
2017-08-10 18:05:19 +0800

    added a path

--- jd/spiders/info.py
+++ jd/spiders/info.py
@@ -2,5 +2,6 @@ import scrapy
 
 # process item info from HTML here
 def parse_info(response):
-    print("info:")
-    pass
+    info = response.xpath('body//div[@class="w"][6]//div[@class="detail"]//div[@class="ETab"]//div[@class="tab-con"]//div[@data-tab="item"][2]//div[@class="Ptable"]/div').extract()
+    print(info)
+    
